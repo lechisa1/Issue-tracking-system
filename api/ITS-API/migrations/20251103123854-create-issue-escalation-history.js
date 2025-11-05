71ae62a8ba999feaa668b4e763092c6094f6a19e
lechisa <lechisabedo123@gmail.com>
2025-11-04 22:18:23 -0800

    issue escaltion added

--- api/ITS-API/migrations/20251103123854-create-issue-escalation-history.js
+++ api/ITS-API/migrations/20251103123854-create-issue-escalation-history.js
@@ -6,7 +6,7 @@ module.exports = {
     await queryInterface.createTable("issue_escalation_history", {
       issue_escalation_history_id: {
         type: Sequelize.UUID,
-        defaultValue: Sequelize.literal("uuid_generate_v4()"),
+        defaultValue: Sequelize.UUIDV4,
         allowNull: false,
         primaryKey: true,
       },
