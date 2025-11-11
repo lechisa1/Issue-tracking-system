d95a7a9c465ed4bc0350e5b0fae3e12ed8e11803
lechisa <lechisabedo123@gmail.com>
2025-11-10 23:40:37 -0800

    role validators updated

--- api/ITS-API/validators/roleValidator.js
+++ api/ITS-API/validators/roleValidator.js
@@ -29,10 +29,21 @@ const createRoleSchema = Joi.object({
       Joi.object({
         sub_role_id: Joi.string()
           .guid({ version: "uuidv4" })
-          .required()
+          .allow(null, "")
           .messages({
             "string.guid": "Each sub_role_id must be a valid UUID",
-            "any.required": "sub_role_id is required for each sub-role",
+          }),
+
+        name: Joi.string()
+          .trim()
+          .when("sub_role_id", {
+            is: Joi.exist().not(null).not(""),
+            then: Joi.forbidden(),
+            otherwise: Joi.required(),
+          })
+          .messages({
+            "any.required": "Name is required when sub_role_id is not provided",
+            "any.unknown": "Name is not allowed when sub_role_id exists",
           }),
 
         permission_ids: Joi.alternatives()
@@ -44,16 +55,10 @@ const createRoleSchema = Joi.object({
             ),
             Joi.string()
           )
-          .optional()
-          .messages({
-            "array.includes": "permission_ids must be a list of UUIDs",
-          }),
+          .optional(),
       })
     )
-    .optional()
-    .messages({
-      "array.base": "sub_roles must be an array",
-    }),
+    .optional(),
 });
 
 // Middleware wrappers
