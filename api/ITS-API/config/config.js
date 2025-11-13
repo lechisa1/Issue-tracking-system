f5b224c54ab885a491742693b55db2af1809af6d
esubalew <esuba6564@gmail.com>
2025-11-13 14:47:34 +0300

    dockeraized backend

--- api/ITS-API/config/config.js
+++ api/ITS-API/config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
@@ -10,7 +10,7 @@ module.exports = {
     dialect: process.env.DB_DIALECT || "postgres",
   },
   test: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
@@ -18,7 +18,7 @@ module.exports = {
     dialect: process.env.DB_DIALECT || "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
