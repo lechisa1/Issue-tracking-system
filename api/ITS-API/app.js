bcd3cce7a239d2c01c5f624a9bee4b0e54e72a7a
esuba-sol <esuba6564@gmail.com>
2025-11-27 13:14:30 +0000

    Backend: production setup + Docker + resolved app.js conflict

--- api/ITS-API/app.js
+++ api/ITS-API/app.js
@@ -101,10 +101,8 @@ app.use(
 
 // ================== CORS Configuration ==================
 const allowedOrigins = [
-  "http://localhost:5173", // <--- ADD THIS
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "http://localhost:4000",
+  "http://196.188.240.103:4037",
+  "http://localhost:4037",
   process.env.FRONTEND_URL,
 ];
 
@@ -186,9 +184,10 @@ app.use((err, req, res, next) => {
 });
 
 // ================== Start App Server ==================
-const appPort = process.env.PORT || 4000;
-appServer.listen(appPort, () => {
-  console.log(` App server running at http://localhost:${appPort}`);
+const appPort = process.env.PORT || 4038;
+appServer.listen(appPort, "0.0.0.0", () => {
+  console.log(`App server running at http://0.0.0.0:${appPort}`);
+
 });
 
 // ================== Socket.IO Setup ==================
@@ -214,8 +213,8 @@ io.on("connection", (socket) => {
 });
 
 const socketPort = process.env.SOCKET_PORT || 5000;
-socketServer.listen(socketPort, () => {
-  console.log(`Socket.IO server running at http://localhost:${socketPort}`);
+socketServer.listen(socketPort, "0.0.0.0", () => {
+  console.log(`Socket.IO server running at http://0.0.0.0:${socketPort}`);
 });
 
 // Make Socket.IO accessible globally
--- api/ITS-API/config/config.js
+++ api/ITS-API/config/config.js
@@ -10,7 +10,7 @@ const pool = {
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
@@ -19,7 +19,7 @@ module.exports = {
     pool,
   },
   test: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
@@ -28,7 +28,7 @@ module.exports = {
     pool,
   },
   production: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
